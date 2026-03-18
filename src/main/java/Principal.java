311afee647e6afd876bce7c435d6fa1c624fd9d7
João Pedro Maziero <Joo Pedro Maziero@10.146.195.34>
2026-03-17 21:02:00 -0300

    init:Início do projeto

---
+++ src/main/java/Principal.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author João Pedro Maziero
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        double base, altura, area;
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base"));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
+        area = (base * altura)/2.0;
+        JOptionPane.showMessageDialog(null,"A área é " + area);
+    }
+}
