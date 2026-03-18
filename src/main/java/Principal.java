84228b93269810e40ddaf0d8fc05f13ff22a6d34
João Pedro Maziero <Joo Pedro Maziero@10.146.195.34>
2026-03-17 21:24:04 -0300

    init:Início do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,7 +7,9 @@ import javax.swing.JOptionPane;
 public class Principal {
 
     public static void main(String[] args) {
+        //Variáveis
         double base, altura, area;
+        //Entrada
         base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base"));
         altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
         area = (base * altura)/2.0;
